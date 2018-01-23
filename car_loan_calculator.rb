36738dc4c38d9b4611c344de9a79c011d9ad92b1
Melissa Brown <melissabrown@gmail.com>
2018-01-23 12:35:01 +0800

    added assignment 1 and 2

---
+++ car_loan_calculator.rb
@@ -0,0 +1,89 @@
+# Input
+# - the loan amount
+# - the Annual Percentage Rate (APR)
+# - the loan duration
+
+# Output
+# - monthly interest rate
+# - loan duration in months
+
+# Algorithim
+# - GET loan amount
+#  -validate Input is a integer
+# - GET anuual interest rate
+#   - validate input
+#   - SET  to decimal
+# SET the monthly_interest_rate => divide annual interest rate by 12
+# - GET loan duration
+# - SET loan duration to months
+# - Calculate  monthly_payment
+# - PRINT monthly_payment and loan duration in months.
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
+def integer?(input)
+  input.to_i.to_s == input
+end
+
+def float?(input)
+  input.to_f.to_s == input
+end
+
+def valid_number?(input)
+  integer?(input) || float?(input)
+end
+loop do
+  prompt('Welcome to the Car Loan Calculator! Please enter your loan amount')
+
+  loan_amount = ''
+
+  loop do
+    loan_amount = Kernel.gets.chomp
+    break if integer?(loan_amount)
+    prompt('Hmmm.... that doesnt look like a valid number')
+  end
+
+  prompt('Please enter your annual interest_rate')
+
+  annual_interest_rate = ''
+
+  loop do
+    annual_interest_rate = Kernel.gets.chomp
+    break if valid_number?(annual_interest_rate)
+    prompt('Hmmm.... that doesnt look like a valid number')
+  end
+
+  prompt('What is the loan duration?')
+
+  loan_duration = ''
+
+  loop do
+    loan_duration = Kernel.gets.chomp
+    break if valid_number?(loan_duration)
+    prompt('Hmmm.... that doesnt look like a valid number')
+  end
+
+  loan_amount = loan_amount.to_i
+  annual_interest_rate = annual_interest_rate.to_f / 100
+  loan_duration = loan_duration.to_i
+  monthly_interest_rate = annual_interest_rate / 12
+  loan_duration_in_months = loan_duration * 12
+
+  # Formula
+  monthly_payment = loan_amount *
+                    (monthly_interest_rate / (1 - (1 + monthly_interest_rate)**(-loan_duration_in_months)))
+
+  total_cost = loan_duration_in_months * monthly_payment
+
+  prompt("Your monthly payment is #{monthly_payment.round(2)}\n
+    for a duration of #{loan_duration_in_months} months or\n
+    #{loan_duration} years. The total cost of your loan is\n
+    $#{total_cost.round(2)}")
+
+  prompt("Do you want to perform another calucation? (Y to calucate again)")
+  answer = Kernel.gets.chomp
+  break unless answer.downcase().start_with?('y')
+end
+prompt("Thank you for using calucator!")
---
+++ rock_paper_scissors.rb
@@ -0,0 +1,81 @@
+VALID_CHOICES = { r: 'rock',
+                  p: 'paper',
+                  s: 'scissors',
+                  sp: 'spock',
+                  l: 'lizard' }
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
+WINNING_MOVES = { rock: ['scissors', 'lizard'],
+                  paper: ['rock', 'spock'],
+                  scissors: ['lizard', 'paper'],
+                  spock: ['rock', 'scissors'],
+                  lizard: ['spock', 'paper'] }
+
+def win?(first, second)
+  WINNING_MOVES[first.to_sym].include?(second)
+end
+
+def display_results(player, computer)
+  if win?(player, computer)
+    prompt("You won this round!")
+  elsif win?(computer, player)
+    prompt('The computer won this round!')
+  else
+    prompt('It is a tie!')
+  end
+end
+
+choice_score = 0
+computer_score = 0
+
+loop do
+  choice = ''
+  loop do
+    prompt("Choose one of the following: #{VALID_CHOICES.values.join(', ')}.
+           Or you can choose #{VALID_CHOICES.keys.join(', ')}.")
+    choice = Kernel.gets().chomp()
+
+    if VALID_CHOICES.values.include?(choice)
+      break
+    elsif VALID_CHOICES.keys.include?(choice.to_sym)
+      choice = VALID_CHOICES[choice.to_sym]
+      break
+    else
+      prompt("That's not a valid choice")
+    end
+  end
+
+  computer_choice = VALID_CHOICES.values.sample
+
+  Kernel.puts("You chose: #{choice}. The computer chose: #{computer_choice}.")
+
+  display_results(choice, computer_choice)
+
+  if win?(choice, computer_choice)
+    choice_score += 1
+  elsif win?(computer_choice, choice)
+    computer_score += 1
+  end
+
+  prompt("Players score is #{choice_score}")
+  prompt("Computers score is #{computer_score}")
+
+  if choice_score == 2
+    prompt("You are the winner!")
+    computer_score = 0
+    choice_score = 0
+  elsif computer_score == 2
+    prompt("The computer is the winner")
+    computer_score = 0
+    choice_score = 0
+  end
+
+  prompt('Do you want to play again?')
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt('Thank you for playing, Goodbye!')
