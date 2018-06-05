62dad254d381d8bf28921c9edcf9af6cb53d0251
Melissa Brown <melissabrown@gmail.com>
2018-06-05 08:59:40 +0800

    finished rubocop on tictac.rb

--- Lesson_6/tictac.rb
+++ Lesson_6/tictac.rb
@@ -1,6 +1,4 @@
-#The logic is simple: if the computer already has 2 in a row, then fill in the 3rd square, as opposed to moving at random.
-
-require 'pry'
+INITIAL_PLAYER = 'choose'
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
@@ -9,13 +7,13 @@ WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # col
                 [[1, 5, 9], [3, 5, 7]]
 
-scores = { player: 0, computer: 0}
+scores = { player: 0, computer: 0 }
 
 def prompt(msg)
   puts "=> #{msg}"
 end
 
-# rubocop: disable Metrics/MethodLength, Metrics AbcSize
+# rubocop:disable Metrics/MethodLength, Metrics/AbcSize
 def display_board(brd, scores)
   system 'clear'
   puts "Your are #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}."
@@ -34,9 +32,8 @@ def display_board(brd, scores)
   puts "      |       |      "
   puts ""
 end
-# rubocop: enable Metrics/MethodLength Metrics/AbcSize
+# rubocop:enable Metrics/MethodLength Metrics/AbcSize
 
-# Returns a hash
 def intialize_board
   new_board = {}
   (1..9).each { |num| new_board[num] = INITIAL_MARKER }
@@ -64,7 +61,8 @@ end
 def player_places_piece!(brd)
   square = ''
   loop do
-    prompt "Choose a position to place a piece:#{joinor(empty_square(brd), ', ')}"
+    prompt "Choose a position to place a piece:
+           #{joinor(empty_square(brd), ', ')}"
     square = gets.chomp.to_i
     break if empty_square(brd).include?(square)
     prompt "Sorry, that's not a valid choice."
@@ -72,11 +70,17 @@ def player_places_piece!(brd)
   brd[square] = PLAYER_MARKER
 end
 
+def sq_five_available?(brd)
+  brd[5] == INITIAL_MARKER
+end
+
 def computer_places_piece!(brd)
   if !!find_at_risk_square(brd, COMPUTER_MARKER) == true
     brd[find_at_risk_square(brd, COMPUTER_MARKER)] = COMPUTER_MARKER
   elsif !!find_at_risk_square(brd, PLAYER_MARKER) == true
     brd[find_at_risk_square(brd, PLAYER_MARKER)] = COMPUTER_MARKER
+  elsif sq_five_available?(brd) == true
+    brd[5] = COMPUTER_MARKER
   else
     square = empty_square(brd).sample
     brd[square] = COMPUTER_MARKER
@@ -88,7 +92,7 @@ def find_at_risk_square(brd, marker)
   WINNING_LINES.select do |line|
     if brd.values_at(*line).count(marker) == 2 &&
        brd.values_at(*line).count(INITIAL_MARKER) == 1
-     threat << line.select {|square| brd[square] == INITIAL_MARKER}
+      threat << line.select { |square| brd[square] == INITIAL_MARKER }
     end
   end
   threat.empty? ? nil : threat[0][0]
@@ -99,7 +103,7 @@ def board_full?(brd)
 end
 
 def someone_won?(brd)
-  !!detect_winner(brd) # output is a boolean
+  !!detect_winner(brd)
 end
 
 def detect_winner(brd)
@@ -113,52 +117,74 @@ def detect_winner(brd)
   nil
 end
 
-def keep_score!(brd,scores)
+def keep_score!(brd, scores)
   if detect_winner(brd) == "Computer"
-     scores[:computer] += 1
+    scores[:computer] += 1
   elsif detect_winner(brd) == "Player"
-     scores[:player] += 1
+    scores[:player] += 1
   end
 end
 
 # improve method take result of declare Winner to boolean form
 def reset_score!(scores)
-  if scores[:player] == 2 || scores[:computer] == 2
-   scores[:player] = 0
-   scores[:computer] = 0
+  if scores[:player] == 5 || scores[:computer] == 5
+    scores[:player] = 0
+    scores[:computer] = 0
   end
 end
-
 
 def declare_winner(scores)
-  if scores[:player] == 2
-    "Player is the Winner"
-  elsif scores[:computer] == 2
-    "Computer is the winner"
+  if scores[:player] == 5
+    "*****!!!! PLAYER IS THE WINNER !!!!*****"
+  elsif scores[:computer] == 5
+    "*****!!!! COMPUTER IS THE WINNER !!!!*****"
   else
-    "This round Player Score = #{scores[:player]} Computer Score = #{scores[:computer]}"
+    "Player Score = #{scores[:player]} Computer Score = #{scores[:computer]}"
   end
 end
 
+def determine_initial_player
+  loop do
+    puts "Who should play first type 'c' for computer and 'p' for player"
+    choice = gets.chomp
+
+    return 'computer' if choice.downcase == 'c'
+    return 'player' if choice.downcase == 'p'
+    prompt("That is not a valid choice")
+  end
+end
+
+def place_piece!(brd, current_player)
+  if current_player == 'player'
+    player_places_piece!(brd)
+  elsif current_player == 'computer'
+    computer_places_piece!(brd)
+  end
+end
+
+def change_turn(current_player)
+  current_player == 'player' ? 'computer' : 'player'
+end
 
 loop do
   board = intialize_board
   reset_score!(scores)
+
+  current_player = determine_initial_player
   loop do
     display_board(board, scores)
 
-    player_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
+    place_piece!(board, current_player)
+    current_player = change_turn(current_player)
 
-    computer_places_piece!(board)
     break if someone_won?(board) || board_full?(board)
   end
 
   keep_score!(board, scores)
-  display_board(board,scores)
+  display_board(board, scores)
   if someone_won?(board)
     prompt "#{detect_winner(board)} won!"
-    prompt "#{declare_winner(scores)}"
+    prompt declare_winner(scores).to_s
   else
     prompt "It's a tie!"
   end
